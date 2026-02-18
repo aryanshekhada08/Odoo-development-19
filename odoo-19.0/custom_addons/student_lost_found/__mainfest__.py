133852c2a68646095d38287b69b3b23fc2eb81eb
Aryan Shekhada <shekhadaaryan37@gmail.com>
2026-02-18 13:29:59 +0530

    second modeule

---
+++ odoo-19.0/custom_addons/student_lost_found/__mainfest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Student Lost & Found',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+    'security/ir.model.access.csv',
+    'security/lost_item_rules.xml',
+    'views/lost_item_views.xml',
+     ],
+    'installable': True,
+    'application': True,
+}
